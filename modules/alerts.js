bf15078a869d14790f299f566d56f18e5e4c719f
Sal Boone <msb@msboone.dev>
2025-04-14 00:06:58 -0700

    Factor out scripts.js into modular code

---
+++ modules/alerts.js
@@ -0,0 +1,12 @@
+/**
+ * @fileOverview this module handles alerts for interactive elements
+ * @module alerts
+ **/
+
+// TODO: add JSdoc for this guy
+function quoteAlert() {
+	console.log("Button Clicked!");
+	alert(
+		"I guess I can kiss heaven goodbye, because it got to be a sin to look this good!",
+	);
+}
---
+++ modules/card.js
@@ -0,0 +1,49 @@
+// TODO: Create a typedef for Card objects
+
+/**
+ * Accepts an array of card objects, and inserts them into the DOM
+ *
+ * @param {Array<Object>} cardData - An array of objects
+ *
+ */
+export function showCards(cardData) {
+	const cardContainer = document.getElementById("card-container");
+	cardContainer.innerHTML = "";
+	const templateCard = document.querySelector(".card");
+
+	// Empty document fragment to be our payload for this data
+	const payload = document.createDocumentFragment();
+
+	// Iterate over our card objects, assemble a card element for each one
+	cardData.forEach((card) => {
+		const title = card.title;
+		const imageURL = card.image_url;
+
+		// Clone the template element, add details for this card
+		const nextCard = templateCard.cloneNode(true); // Copy the template card
+		editCardContent(nextCard, title, imageURL); // Edit title and image
+
+		// Add card to payload
+		payload.appendChild(nextCard);
+	});
+
+	cardContainer.appendChild(payload);
+}
+
+function editCardContent(card, newTitle, newImageURL) {
+	card.style.display = "block";
+
+	const cardHeader = card.querySelector("h2");
+	cardHeader.textContent = newTitle;
+
+	const cardImage = card.querySelector("img");
+	cardImage.src = newImageURL;
+	cardImage.alt = newTitle + " Poster";
+	console.log("new card:", newTitle, "- html: ", card);
+}
+
+// TODO: Enable this once we circle back on CRUD
+// function removeLastCard() {
+// 	titles.pop(); // Remove last item in titles array
+// 	showCards(); // Call showCards again to refresh
+// }
