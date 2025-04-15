de088f53e037256a61923772ee8b8026ac9b96a4
Sal Boone <msb@msboone.dev>
2025-04-14 23:48:34 -0700

    Add basic class for card objects with JSDoc comment

--- modules/card.js
+++ modules/card.js
@@ -1,4 +1,88 @@
-// export class Card {}
+/**
+ * @fileOverview Module for Card objects and applicable methods that act on them
+ * @module card
+ */
+
+/**
+ * Represents a card element within the catalogue
+ * @class
+ */
+export class Card {
+	/**
+	 * Constructor method for Card objects
+	 * @constructor
+	 * @param {Object} cardData - JSON Object containing the card data TODO: Create typedef with the json object structure
+	 */
+	constructor(cardData) {
+		// TODO: Use camel case instead of snake case ;)
+		const { title, flavor_text, image_url, mtg_meta, scryfall_meta } = jsonData;
+
+		/**
+		 * Title of the card.
+		 * @type {!string}
+		 */
+		this.title = title;
+
+		/**
+		 * Flavor text on the card, if any, given this specific printing.
+		 * @type  {?string}
+		 */
+		this.flavor_text = flavor_text;
+
+		/**
+		 * Url to an image for the art of this card.
+		 * @type {!string}
+		 */
+		this.image_url = image_url;
+
+		/**
+		 * Metadata regarding this cards rulings/properties as an MTG card
+		 * @type {?Array<Object>}
+		 */
+		this.mtg_meta = mtg_meta;
+
+		/**
+		 * Metadata regarding the entry for this card in the Scryfall database.
+		 * @type {?Array<Object>}
+		 */
+		this.scryfall_meta = scryfall_meta;
+	}
+}
+
+/**
+ * Function that accepts a card element, and updates it with the contents of a new Card object
+ * @param {!Object} cardElement - The card element within the DOM to update
+ * @param {!Card} cardObject - The card object to populate the element from
+ * @returns {!Object}
+ */
+function editCardContent(cardElement, cardObject) {
+	const newTitle = cardObject.title;
+	const newImageURL = cardObject.image_url;
+
+	cardElement.style.display = "block";
+	cardElement.querySelector("h2").textContent = newTitle;
+
+	const cardImage = cardElement.querySelector("img");
+	cardImage.src = newImageURL;
+	cardImage.alt = `Card Art - ${newTitle}`;
+	console.log("new card:", newTitle, "- html: ", cardElement);
+
+	return cardElement;
+}
+
+/**
+ * Accepts a card object, and returns a DOM element as a render of the object.
+ * @param {!Card} cardObject - A well-formed Card object
+ * @returns {!Object}
+ */
+function renderCard(cardObject) {
+	// Copy an existing card as a template, and populate it with the data from the cardObject
+	// HACK: This is gonna cause problems with updates if we rely on this function.. probably?
+	const templateCard = document.querySelector(".card").cloneNode(true);
+	const newCard = editCardContent(templateCard, cardObject);
+
+	return newCard;
+}
 
 /**
  * Accepts an array of card objects, and inserts them into the DOM
@@ -7,39 +91,23 @@
  *
  */
 export function showCards(cardData) {
+  // Get content area for cards, and clear existing elements
 	const cardContainer = document.getElementById("card-container");
 	cardContainer.innerHTML = "";
-	const templateCard = document.querySelector(".card");
 
 	// Empty document fragment to be our payload for this data
 	const payload = document.createDocumentFragment();
 
 	// Iterate over our card objects, assemble a card element for each one
 	cardData.forEach((card) => {
-		const title = card.title;
-		const imageURL = card.image_url;
-
-		// Clone the template element, add details for this card
-		const nextCard = templateCard.cloneNode(true); // Copy the template card
-		editCardContent(nextCard, title, imageURL); // Edit title and image
+    // FIX: Refactor this, accept a 'CardCollection' sort of thing... might need to update card template
+		const renderedCard = renderCard(card);
 
 		// Add card to payload
-		payload.appendChild(nextCard);
+		payload.appendChild(renderedCard);
 	});
 
 	cardContainer.appendChild(payload);
-}
-
-function editCardContent(card, newTitle, newImageURL) {
-	card.style.display = "block";
-
-	const cardHeader = card.querySelector("h2");
-	cardHeader.textContent = newTitle;
-
-	const cardImage = card.querySelector("img");
-	cardImage.src = newImageURL;
-	cardImage.alt = newTitle + " Poster";
-	console.log("new card:", newTitle, "- html: ", card);
 }
 
 // TODO: Enable this once we circle back on CRUD
