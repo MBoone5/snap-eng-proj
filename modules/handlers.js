f150cd465daefdbc1323f371fad4cb5791581020
Sal Boone <msb@msboone.dev>
2025-04-16 02:25:51 -0700

    Add handlers module

---
+++ modules/handlers.js
@@ -0,0 +1,35 @@
+/// <reference path="../types.js" />
+/// <reference path="./card.js" />
+
+/**
+ *
+ * @param {Event} event
+ */
+const handleSortOrFilter = (event) => {
+  return;
+}
+
+/**
+ *
+ * @param {Event} event
+ */
+const clearFilters = (event) => {
+
+}
+
+/**
+ *
+ * @param {Event} event
+ */
+const handleCreateCard = (event) => {
+  return;
+}
+
+/**
+ *
+ * @param {Event} event
+ */
+const handleDeleteCard = (event) => {
+  return;
+}
+
